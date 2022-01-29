7a19846660f126b8e7fa82908df1553c9c695b98
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-29 10:25:04 +0530

    Home Page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: [],
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
